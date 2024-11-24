9383394c519e0429340fe181ad8429992cd447e4
liamreeves34 <liamreeves34@gmail.com>
2024-11-23 22:38:09 -0600

    I forgot to close h3

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -7,7 +7,8 @@ export function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            If you would like more information about Data Science, Search "Data Science memes" in your browser.
+          If you would like more information about Data Science, Search "Data Science memes" in your browser.
+          </h3>
         </div>
       </Container>
     </footer>
